4b6b68b2f28f12cd77b6d9cad2c7efc5fa74f857
Nick Tindall <nick@tindall.id.au>
2014-07-27 16:14:42 +1000

    Finish implementing trickle ICE, increase dependency versions

--- AppMetadata.js
+++ AppMetadata.js
@@ -4,6 +4,6 @@
 	Application-wide metadata is stored here for reuse
 */
 module.exports = {
-	FRONTEND_VERSION: 29,
+	FRONTEND_VERSION: 30,
     SIGNALLING_SERVERS: JSON.parse('/* @echo SIGNALLING_SERVERS */')
 }
